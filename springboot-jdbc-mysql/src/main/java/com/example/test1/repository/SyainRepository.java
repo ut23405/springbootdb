2d90c4ac86c9c4d2b06d2e8af1773654b5363629
ut23405 <ut23405@gmail.com>
2022-01-16 14:34:57 +0900

    mysql-jdbc-select

--- springboot-jdbc-mysql/src/main/java/com/example/test1/repository/SyainRepository.java
+++ springboot-jdbc-mysql/src/main/java/com/example/test1/repository/SyainRepository.java
@@ -1,5 +1,6 @@
 package com.example.test1.repository;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -14,14 +15,16 @@ public class SyainRepository {
 	private final JdbcTemplate jdbcTemplate;
 
 	public List<SyainDto> getAll() {
-		String sql = "select id,name,romaji from syain";
+		String sql = "select id,name,romaji,created_at,updated_at from syain";
 		List<Map<String, Object>> syainList = jdbcTemplate.queryForList(sql);
 		List<SyainDto> list = new ArrayList<>();
 		for (Map<String, Object> syain : syainList) {
 			list.add(new SyainDto(
 					(int) syain.get("id"),
 					(String) syain.get("name"),
-					(String) syain.get("romaji")));
+					(String) syain.get("romaji"),
+					(LocalDateTime) syain.get("created_at"),
+					(LocalDateTime) syain.get("updated_at")));
 		}
 		return list;
 	}
