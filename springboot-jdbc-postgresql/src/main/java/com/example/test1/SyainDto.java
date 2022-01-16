926689f25167496fc3b81d9b02e5c1a7203ddab4
ut23405 <ut23405@gmail.com>
2022-01-16 16:33:58 +0900

    postgresql-jdbc-select

--- springboot-jdbc-postgresql/src/main/java/com/example/test1/SyainDto.java
+++ springboot-jdbc-postgresql/src/main/java/com/example/test1/SyainDto.java
@@ -1,12 +1,15 @@
 package com.example.test1;
 
+import java.util.Date;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
 public class SyainDto {
-    private Integer id;
+    private int id;
     private String name;
     private String romaji;
+    private Date createdAt;
+    private Date updatedAt;
 }
--- springboot-jdbc-postgresql/src/main/java/com/example/test1/repository/SyainRepository.java
+++ springboot-jdbc-postgresql/src/main/java/com/example/test1/repository/SyainRepository.java
@@ -1,6 +1,7 @@
 package com.example.test1.repository;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -14,14 +15,16 @@ public class SyainRepository {
 	private final JdbcTemplate jdbcTemplate;
 
 	public List<SyainDto> getAll() {
-		String sql = "select id,name,romaji from syain";
+		String sql = "select id,name,romaji,created_at,updated_at from syain";
 		List<Map<String, Object>> syainList = jdbcTemplate.queryForList(sql);
 		List<SyainDto> list = new ArrayList<>();
 		for (Map<String, Object> syain : syainList) {
 			list.add(new SyainDto(
 					(int) syain.get("id"),
 					(String) syain.get("name"),
-					(String) syain.get("romaji")));
+					(String) syain.get("romaji"),
+					(Date) syain.get("created_at"),
+					(Date) syain.get("updated_at")));
 		}
 		return list;
 	}
